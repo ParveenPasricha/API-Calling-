25b7fbf4d4cedf3d7adbe3120693baac0d036d75
ParveenPasricha <pasricha.parveen@yahoo.in>
2024-10-23 13:03:03 +0530

    Movement Page

--- src/Pages/Movement.jsx
+++ src/Pages/Movement.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Movement() {
   return (
-    <div className='bg-red-600 font-semibold p-4 flex'>
-        <div className='text-left'>
-          <h1>Portfolio</h1>
-        </div>
-        <div>
-            <Link className='m-6 hover:bg-black hover:text-white p-4' to="/" >Home</Link>
-            <Link className='m-6 hover:bg-black hover:text-white p-4' to="/contact">Contact</Link>
-            <Link className='m-6 hover:bg-black hover:text-white p-4' to="/movement">Movement</Link>
-            <Link className='m-6 hover:bg-black hover:text-white p-4' to="/qualification">Qualification</Link>
-        </div>
-        <div>
-          <img src=''></img>
-        </div>
+    <>
+    <div className='bg-blue-600 font-semibold p-4 flex' >
+      <div className='text-left '>
+        <h1>Portfolio</h1>
+      </div>
+      <div>
+        <Link className='m-6 hover:bg-black hover:text-white p-4' to="/">Home</Link>
+        <Link className='m-6 hover:bg-black hover:text-white p-4' to="/contact">Contact</Link>
+        <Link className='m-6 hover:bg-black hover:text-white p-4' to="/movement">Movement</Link>
+        <Link className='m-6 hover:bg-black hover:text-white p-4' to="/qualification">Qualification</Link>
+      </div>
     </div>
-    
-  )
+    hello World
+    </>
+  );
 }
 
-export default Movement
+export default Movement;
